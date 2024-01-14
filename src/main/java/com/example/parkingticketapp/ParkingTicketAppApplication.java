53e6f4535b9dd57e82409bdc731e7e7a33b687b0
Jwoliv <bohdanlishchuk0@gmail.com>
2024-01-14 12:05:21 +0200

    Init commit

---
+++ src/main/java/com/example/parkingticketapp/ParkingTicketAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkingticketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingTicketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingTicketAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parkingticketapp/ParkingTicketAppApplicationTests.java
@@ -0,0 +1,8 @@
+package com.example.parkingticketapp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingTicketAppApplicationTests {
+
+}
